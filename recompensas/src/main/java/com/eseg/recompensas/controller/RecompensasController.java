81947e2edb0adef0246c65d54d1b31296958a4d9
Audrei Plume <semideiaa0@gmail.com>
2025-11-17 18:00:55 -0300

    Add controller recompensa

---
+++ recompensas/src/main/java/com/eseg/recompensas/controller/RecompensasController.java
@@ -0,0 +1,52 @@
+package com.eseg.recompensas.controller;
+
+import com.eseg.recompensas.model.Recompensa;
+import com.eseg.recompensas.service.RecompensaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recompensas")
+public class RecompensasController {
+    private final RecompensaService recompensaService;
+
+    public RecompensasController(RecompensaService recompensaService) {
+        this.recompensaService = recompensaService;
+    }
+
+    //Listar todas as recompensas
+    @GetMapping
+    public List<Recompensa> listarTodas(){
+        return recompensaService.listarTodos();
+    }
+
+    //Buscar recompensa por ID
+    @GetMapping("/id")
+    public ResponseEntity<Recompensa> buscarPorId(Long id) {
+        Recompensa recompensa = recompensaService.buscaPorId(id);
+        return recompensa != null ? ResponseEntity.ok(recompensa) : ResponseEntity.notFound().build();
+    }
+
+    //Criar recompensa
+    @PostMapping
+    public ResponseEntity<Recompensa> criar(@RequestBody Recompensa novaRecompensa){
+        Recompensa criada = recompensaService.criarRecompensa(novaRecompensa);
+        return ResponseEntity.status(HttpStatus.CREATED).body(criada);
+    }
+
+    @PutMapping("/id")
+    public ResponseEntity<Recompensa> atualizar(@PathVariable Long id, @RequestBody Recompensa recompensaAtualizada) {
+        Recompensa atualizada = recompensaService.editarRecompensa(id, recompensaAtualizada);
+        return ResponseEntity.ok(atualizada);
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        recompensaService.deletarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+}
+
